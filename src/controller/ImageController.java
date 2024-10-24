7f07c4aed08f083e0e9b106a4bf63fad56afecf8
Parin Shah <99parinshah@gmail.com>
2024-10-24 11:32:38 -0400

    added javadoc for controller

--- src/controller/ImageController.java
+++ src/controller/ImageController.java
@@ -1,7 +1,15 @@
 package controller;
 
-
+/**
+ * This is an Interface for the Controller. This class executes the commands provided for image
+ * loading, manipulation or storing.
+ */
 public interface ImageController {
 
+  /**
+   * A function to accept the inputs from the view or user.
+   *
+   * @param scriptFilePath contains the location of the list of commands.
+   */
   void runScript(String scriptFilePath);
 }
